b7ca89b34f4eaa13f7ee04598df225dda5a34bad
Lazar Dilov <ldilov@isimarkets.com>
2021-12-12 07:13:01 +0200

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,11 +8,13 @@ import Menu from './MenuComponent';
 import DishDetails from './DishDetailsComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 import { DISHES } from '../data/mock/dishes';
 import { COMMENTS } from '../data/mock/comments';
 import { LEADERS } from '../data/mock/leaders';
 import { PROMOTIONS } from '../data/mock/promotions';
+
 
 class Main extends Component {
 
@@ -62,6 +64,7 @@ class Main extends Component {
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />} />
             <Route exact path='/contactus' component={Contact} />} />
+            <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Redirect to="/home" />
           </Switch>
